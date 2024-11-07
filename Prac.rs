dbbd0de32cab501cc7d8163af1a5cb36a2825670
NazarFrunas <nazarfrynas@gmail.com>
2024-11-07 02:04:37 +0200

    Prac 9

--- Prac.rs
+++ Prac.rs
@@ -1,18 +1,29 @@
-fn change_case(s: &str) -> String {
-    s.chars()
-        .map(|c| {
-            if c.is_uppercase() {
-                c.to_lowercase().to_string()
-            } else {
-                c.to_uppercase().to_string()
-            }
-        })
-        .collect()
+fn is_prime(n: u32) -> bool {
+    if n <= 1 {
+        return false;
+    }
+    if n == 2 {
+        return true;
+    }
+    if n % 2 == 0 {
+        return false;
+    }
+    let limit = (n as f64).sqrt() as u32;
+    for i in 3..=limit {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
 }
-
 fn main() {
-    let text = "Hello, World!";
+    let numbers = [1, 2, 3, 4, 5, 16, 17, 18, 19, 20];
 
-    let changed = change_case(text);
-    println!("Changed case: {}", changed);
+    for &num in &numbers {
+        if is_prime(num) {
+            println!("{} is prime", num);
+        } else {
+            println!("{} is not prime", num);
+        }
+    }
 }
